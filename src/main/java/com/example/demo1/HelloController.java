7ec3ca0b2c5c676c7af9f284e94f19f59a9ecb5b
Peter Dz <piotreq_46@wp.pl>
2022-06-10 16:15:37 +0200

    sql

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -34,7 +34,7 @@ public class HelloController {
         if(pracownik!=null){
             errorLabel.setText("");
             if(pracownik.isAdmin()){
-                
+
             }
 
         }
